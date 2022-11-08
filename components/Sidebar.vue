7c18e265920fd4e8fde7e699156a4aa9db20f086
emircandemr <72731296+emircandemr@users.noreply.github.com>
2022-11-08 21:51:32 +0300

    updated : sidebar

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -3,38 +3,27 @@
 </script>
 
 <template>
-    <div class="w-60 h-screen bg-[#121212]">
-        <div class="w-full h-20 text-2xl font-bold text-white mr-5 flex flex-col justify-center items-center mt-10 ">
-            <img src="https://user-images.githubusercontent.com/72731296/200189155-471a2374-a590-4451-b691-31505eae67d4.png" alt="">
-        </div>
-        <div class="w-full h-3/4  flex flex-col items-center text-white">
-            <ul class=" w-11/12 py-1 rounded-lg hover:bg-gray-900" v-for="i in 5">
-                <li class="my-2">
-                    <NuxtLink  to="/" class="flex" >
-                        <div class="w-12 flex justify-center items-center">
-                            <span class="material-symbols-rounded">
-                                home
-                            </span>
-                        </div>
-                        <div>
-                            Home
-                        </div>
-                    </NuxtLink>
-                </li>
-            </ul>
-        </div>
-        <div class="w-full h-24 flex justify-center items-center ">
-            <div class=" mr-5 ">
-                <img class="w-14 h-14 rounded-full" src="https://flxt.tmsimg.com/assets/587180_v9_bb.jpg" alt="">
+    <div class="w-40 h-screen flex items-center justify-center">
+        <div class="w-28 h-[90%] bg-[#0C0B0B] rounded-xl shadow-2xl ">
+            <div class="w-full h-20 text-2xl font-bold text-white mr-5 flex flex-col justify-center items-center mt-10 ">
+                <h1>P.</h1>
             </div>
-            <div class="avatar__name">
-                <div class="avatar__name--name">
-                    <span class="text-white font-bold">
-                        John Doe
-                    </span>
-                </div>
-                </div>
-        </div>  
+            <div class="w-full h-3/4  flex flex-col items-center text-white">
+                <ul class="py-1 px-5 rounded-lg hover:bg-gray-900" v-for="i in 5">
+                    <li class="my-5">
+                        <NuxtLink  to="/" class="flex" >
+                            <div class="flex justify-center items-center">
+                                <span class="material-symbols-rounded">
+                                    home
+                                </span>
+                            </div>
+                        </NuxtLink>
+                    </li>
+                </ul>
+            </div>
+            
+
+        </div>
     </div>
 </template>
 
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,11 @@
 <script setup>
-    import Navbar from "~/components/Navbar.vue";
 </script>
 
 <template>
     <div class="main">
         <Sidebar/>
         <div class="main__content" >
-            <Navbar/>
+            <Header> </Header>
             <slot></slot>  
         </div>
     </div>
@@ -16,12 +15,11 @@
 <style scoped>
 
 .main {
-    @apply w-full h-screen bg-[#121212] flex  items-center
+    @apply w-full h-screen bg-gray-50 flex  items-center
 }
 
 .main__content{
-  @apply  text-white bg-[#0f0f0f] w-11/12 flex flex-col items-center rounded-3xl ;
-  height: 95%;
+  @apply  bg-gray-50 w-full h-screen flex flex-col items-center ;
 }
 
 
