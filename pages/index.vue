4c11f36dfcd363414a35a626c7b8d60eb6485957
emircandemr <72731296+emircandemr@users.noreply.github.com>
2022-11-19 23:57:52 +0300

    ✨ refactor : index page in button comp.

--- pages/index.vue
+++ pages/index.vue
@@ -37,35 +37,16 @@ const selectFilter = computed(() => {
             <div class="w-[55%] min-h-[85%] mt-5 ml-7 px-5 flex flex-col text-white overflow-y-auto">
                 <h1 class=" text-2xl">Tasks</h1>
                 <div class="w-full mt-3 text-start">
-                    <button v-for="statu in status" @click="filterHandler(statu)" class="px-3 mr-2 py-1 cursor-pointer hover:text-[#5293ee]">{{statu}}</button>
+                    <SharedButton 
+                    v-for="statu in status"
+                    :handler = "() => filterHandler(statu)"
+                    :text = "statu"
+                    class="px-3 mr-2 py-1 cursor-pointer hover:text-[#5293ee]"
+                    >
+                    </SharedButton>
                 </div>
                 <CardList v-for="task in selectFilter" :task="task"  ></CardList>
-                <!-- <div v-for="item in selectFilter" class="w-full h-20 mt-5 py-3 bg-[#212121] rounded-2xl flex items-center"> -->
-                    <!-- <img class="w-14 h-14 rounded-md ml-2" src="../assets/img/task.png" alt="">
-                    <div class="w-[50%] h-14 ml-3 flex flex-col justify-center">
-                        <h2 class="text-sm">{{item.title}}</h2>
-                        <p class="text-sm truncate">{{item.description}}</p>
-                    </div>
-                    <div class="w-[10%] h-14 ml-3 flex flex-col items-center justify-center">
-                        <span class="text-sm material-symbols-outlined">
-                        favorite
-                        </span>
-                        <span class="mt-1 text-xs ">
-                        +{{item.point}}
-                        </span>
-                    </div>
-                    <div class="w-[10%] h-14 ml-5 flex flex-col items-center justify-center">
-                        <span class="text-sm material-symbols-outlined">
-                        star
-                        </span>
-                        <span class="mt-1 text-xs ">
-                        {{item.category}}
-                        </span>
-                    </div>
-                    <div class="w-[10%] h-8 rounded-lg bg-[#22559c]  cursor-pointer ml-5 text-sm flex items-center justify-center">
-                        Inspect
-                    </div> -->
-                <!-- </div> -->
+                
             </div>
             <Statistic></Statistic>
         </div>
@@ -94,3 +75,30 @@ const selectFilter = computed(() => {
 }
 
 </style>
+
+<!-- <div v-for="item in selectFilter" class="w-full h-20 mt-5 py-3 bg-[#212121] rounded-2xl flex items-center"> -->
+                    <!-- <img class="w-14 h-14 rounded-md ml-2" src="../assets/img/task.png" alt="">
+                    <div class="w-[50%] h-14 ml-3 flex flex-col justify-center">
+                        <h2 class="text-sm">{{item.title}}</h2>
+                        <p class="text-sm truncate">{{item.description}}</p>
+                    </div>
+                    <div class="w-[10%] h-14 ml-3 flex flex-col items-center justify-center">
+                        <span class="text-sm material-symbols-outlined">
+                        favorite
+                        </span>
+                        <span class="mt-1 text-xs ">
+                        +{{item.point}}
+                        </span>
+                    </div>
+                    <div class="w-[10%] h-14 ml-5 flex flex-col items-center justify-center">
+                        <span class="text-sm material-symbols-outlined">
+                        star
+                        </span>
+                        <span class="mt-1 text-xs ">
+                        {{item.category}}
+                        </span>
+                    </div>
+                    <div class="w-[10%] h-8 rounded-lg bg-[#22559c]  cursor-pointer ml-5 text-sm flex items-center justify-center">
+                        Inspect
+                    </div> -->
+                <!-- </div> -->
