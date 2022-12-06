e98bf2b2f78a8bc5403f21b283973703ea82d531
emircandemr <72731296+emircandemr@users.noreply.github.com>
2022-12-06 22:41:39 +0300

    ⭐ update : bug fixed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode' ],
+    modules: ['@nuxtjs/tailwindcss' ],
     buildModules: ['@pinia/nuxt','@nuxtjs/moment'],
     tailwindcss: {
         cssPath : '~/assets/css/tailwind.css',
